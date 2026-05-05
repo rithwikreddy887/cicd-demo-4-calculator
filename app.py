568b0ce0c8db98ce9c0af08ea018e15fe9aa8daa
Your Name <rithwikreddy887@gmail.com>
2026-05-05 02:39:28 +0000

    calculator logic for testing real logic

---
+++ app.py
@@ -0,0 +1,7 @@
+from calculator import add, subtract, multiply, divide
+
+if __name__ == "__main__":
+    print("Add:", add(5, 3))
+    print("Subtract:", subtract(5, 3))
+    print("Multiply:", multiply(5, 3))
+    print("Divide:", divide(5, 1))
