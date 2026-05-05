c13dd74a0e64c8e0c94ab2170ad5bf409366867f
Your Name <rithwikreddy887@gmail.com>
2026-05-05 02:47:13 +0000

    fixing error

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,8 @@
-import pytest
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from calculator import add, subtract, multiply, divide
 
 def test_add():
